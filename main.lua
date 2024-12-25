cd6405009e40287695916e682f1c51e86f8d234d
KhánhDz <vichuyen.123@gmail.com>
2024-12-25 20:35:09 +0700

    update main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+--- Module chính xử lý các chức năng mà Java sẽ trả callback
+-- @module MainLuaByKhanhDz
+-- @author KhanhDzai - https://www.facebook.com/khanhdepzai.pro/
+-- @version 1.0
+MainLuaByKhanhDz = {
+    
+}
